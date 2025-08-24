c3e2620277a6733d91c8af140a3b216fec37c7d3
Jahsfreedman Moffat <xcoder2442@gmail.com>
2025-08-24 17:10:54 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { cn } from '@/lib/utils';
 import { ClientProviders } from './client-providers';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 
 export const metadata: Metadata = {
     title: 'Code-X',
@@ -16,15 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
-          rel="stylesheet"
-        ></link>
-      </head>
-      <body className={cn('font-body antialiased')}>
+      <body className={cn('font-body antialiased', inter.variable)}>
         <ClientProviders>{children}</ClientProviders>
       </body>
     </html>
